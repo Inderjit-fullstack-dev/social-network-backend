705b438ad071a0fb3d2d9ce7d1b9e07a0791f931
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2024-06-01 18:47:54 +0530

    basic post functionality done

---
+++ SocialNetwork.Infrastructure/Helpers/DateTimeExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace SocialNetwork.Infrastructure.Helpers
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToAgoString(this DateTime dateTime)
+        {
+            var timeSpan = DateTime.Now.Subtract(dateTime);
+
+            string result;
+            if (timeSpan <= TimeSpan.FromSeconds(60))
+            {
+                result = string.Format("{0} seconds ago", timeSpan.Seconds);
+            }
+            else if (timeSpan <= TimeSpan.FromMinutes(60))
+            {
+                result = timeSpan.Minutes > 1 ?
+                    string.Format("about {0} minutes ago", timeSpan.Minutes) :
+                    "about a minute ago";
+            }
+            else if (timeSpan <= TimeSpan.FromHours(24))
+            {
+                result = timeSpan.Hours > 1 ?
+                    string.Format("about {0} hours ago", timeSpan.Hours) :
+                    "about an hour ago";
+            }
+            else if (timeSpan <= TimeSpan.FromDays(30))
+            {
+                result = timeSpan.Days > 1 ?
+                    string.Format("about {0} days ago", timeSpan.Days) :
+                    "yesterday";
+            }
+            else if (timeSpan <= TimeSpan.FromDays(365))
+            {
+                result = timeSpan.Days > 30 ?
+                    string.Format("about {0} months ago", timeSpan.Days / 30) :
+                    "about a month ago";
+            }
+            else
+            {
+                result = timeSpan.Days > 365 ?
+                    string.Format("about {0} years ago", timeSpan.Days / 365) :
+                    "about a year ago";
+            }
+
+            return result;
+        }
+    }
+}
